46ada1fba96272b22739eed58d9edc5891a90122
Jason Adriel <143072575+subtle64@users.noreply.github.com>
2023-09-18 19:24:22 +0700

    Added ASG2 files.

---
+++ ASG2/library_example.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SUNIB Library</title>
+    <link rel="stylesheet" href="library.css">
+</head>
+<body>
+    <nav>
+        <img src="" alt="SuNiB">
+        <ul>
+           <a href=""><li>Search</li></a>
+           <a href=""><li>Insert Data</li></a>
+           <a href=""><li>Edit Data</li></a>
+        </ul>
+    </nav>
+    <div class = "search-bar">
+        <input type="text">
+        <input type="submit">
+    </div
+</body>
+</html>
