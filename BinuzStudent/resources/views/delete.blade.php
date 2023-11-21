9fd18030faeba68d164b598812c9e302c7784776
alberttuwan1 <105161746+alberttuwan1@users.noreply.github.com>
2023-11-21 10:36:10 +0700

    Delete BinuzStudent directory

--- BinuzStudent/resources/views/delete.blade.php
+++
@@ -1,8 +0,0 @@
-<marquee behavior="alternate" direction="">
-    <marquee behavior="alternate" direction="down" height=100%>
-        <h3 style='text-align:center'>
-            You have sucessfuly deleted someone from existence <br>
-            <a href="/">Go home</a>
-        </h3>
-    </marquee>
-</marquee>
